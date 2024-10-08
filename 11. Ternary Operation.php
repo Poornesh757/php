d1a0d3a20e7d9ce318c0d85a4986debf57590b27
Poornesh757 <poornesh757@gmail.com>
2024-10-08 21:58:46 +0530

    php

---
+++ 11. Ternary Operation.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // Example of a ternary operation
+        $age = 20;
+        $can_vote = ($age >= 18) ? "Yes" : "No";
+
+        echo "Can vote? $can_vote"; // Outputs: Can vote? Yes
+    ?>
+</body>
+</html>
